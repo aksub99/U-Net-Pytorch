6d72eed36dfe7fac728d726e278f71f170ef3a66
aksub99 <akshaysubramanian99@gmail.com>
2019-03-04 14:35:17 +0530

    Implement up and right_out

--- U-net_modules_defn.py
+++ U-net_modules_defn.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class conv_norm_relu(nn.Module):
     # This class defines a double conv norm relu block
     def __init__(self, channels_in, channels_out):
@@ -18,6 +19,7 @@ class conv_norm_relu(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
+
 class right_in(nn.Module):
     def __init__(self, channels_in, channels_out):
         super(right_in, self).__init__()
@@ -26,8 +28,10 @@ class right_in(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
+
 class down(nn.Module):
     def __init__(self, channels_in, channels_out):
+        super(down, self).__init__()
         self.conv = nn.Sequential(
             nn.MaxPool2d(2, 2),
             conv_norm_relu(channels_in, channels_out)
@@ -36,8 +40,29 @@ class down(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
+
 class up(nn.Module):
-    raise(NotImplementedError)
+    def __init__(self, channels_in, channels_out):
+        super(up, self).__init__()
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = conv_norm_relu(channels_in, channels_out)
+    
+    # Assuming the input is (C, H, W)
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        diffH = x2.size()[0] - x1.size()[0]
+        diffW = x2.size()[1] - x2.size()[1]
+        x1 = F.pad(x1,(diffW // 2, diffW - diffW//2, diffH // 2, diffH - diffH//2))
+        x = torch.cat([x2, x1], dim=1)
+        x = self.conv(x)
+        return x
+
 
 class right_out(nn.Module):
-    raise(NotImplementedError)        
+    def __init__(self, channels_in, channels_out):
+        super(right_out, self).__init__()
+        self.conv = nn.Conv2d(channels_in, channels_out, 1)
+
+    def forward(self, x):
+        return self.conv(x)
+
